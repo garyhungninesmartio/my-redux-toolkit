9802c063456f09ae91ae22fa9c44cde2f83f007d
Gary Hung <gary.hung@ninesmart.io>
2023-09-04 15:52:29 +0800

    Fix immer reducer should not return any value

--- src/store/index.js
+++ src/store/index.js
@@ -2,25 +2,35 @@ import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 // Also known as 'slices' of state
 const counterSlice = createSlice({
-  name: 'counter',
+  name: "counter",
   initialState: {
-    value : 0
+    value: 0,
   },
   reducers: {
     // immer library allows us to pass a 'mutated' state
-    increment: state => state.value += 1,
-    decrement: state => state.value -= 1,
-    incrementByAmount : (state, action) => state.value += action.payload,
-    decrementByAmount : (state, action) => state.value -= action.payload,
-  }
-})
+    // WARN: don't return anything otherwise immer cannot generate new state for us.
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+    decrementByAmount: (state, action) => {
+      state.value -= action.payload;
+    },
+  },
+});
 
 export const selectCount = (state) => state.count.value;
-export const { increment, decrement, incrementByAmount, decrementByAmount } = counterSlice.actions;
+export const { increment, decrement, incrementByAmount, decrementByAmount } =
+  counterSlice.actions;
 
 export default configureStore({
   // The property names here will be the names in the final state e.g. state.count
   reducer: {
     count: counterSlice.reducer, // we specify we want this slice of reducer to handle state.count changes
-  }
-})
+  },
+});
