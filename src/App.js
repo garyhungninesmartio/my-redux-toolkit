9fcf852b16b4eacc9424c3825be874cdeba3b4ae
Gary Hung <gary.hung@ninesmart.io>
2023-09-04 15:50:36 +0800

    Add count slice

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, selectCount } from "./store";
 
 function App() {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+
+  const handleAddButton = () => dispatch(increment());
+  const handleSubButton = () => dispatch(decrement());
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p>Current value: {count}</p>
+        <div style={{flexDirection: 'row'}}>
+        <button onClick={handleAddButton}>Add</button>
+        <button onClick={handleSubButton}>Subtract</button>
+        </div>
       </header>
     </div>
   );
