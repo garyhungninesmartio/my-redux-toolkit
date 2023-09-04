c6a17c653da18cfd5a06161f8bf95ef618665d2d
Gary Hung <gary.hung@ninesmart.io>
2023-09-04 16:00:38 +0800

    Add comment

--- src/App.js
+++ src/App.js
@@ -15,11 +15,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
         <p>Current value: {count}</p>
         <div style={{flexDirection: 'row'}}>
         <button onClick={handleAddButton}>Add</button>
         <button onClick={handleSubButton}>Subtract</button>
+        {/* Can also directly use in onClick */}
         <button onClick={() => dispatch(reset())}>Reset</button>
         </div>
       </header>
